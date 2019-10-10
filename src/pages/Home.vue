427a024fd906ce39fa6c79822afc01942e43acab
joewalker <assassin.joe@hotmail.com>
2019-10-10 20:26:03 +0700

    init project

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <Navbar></Navbar>
+</template>
+<script>
+  import Navbar from '../components/Navbar.vue'
+
+  export default {
+    name: 'HomePage',
+    components: {
+      Navbar
+    }
+  }
+</script>
